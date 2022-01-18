847c3c4718f81b4fe5fcbffd0c963f6ec00f9773
EnderWarik <Steam1117@mail.ru>
2022-01-18 12:33:25 +1000

    init

---
+++ Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ResourseJWT
+{
+    public class Product
+    {
+        public Product() { }
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,78 @@
+﻿
+using AuthorizationWithJWT.Library;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ResourseJWT
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; } 
+
+        public Startup(IConfiguration configuration)
+        {  
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            
+            var authOptionsConfiguration = Configuration.GetSection("Auth").Get<AuthOptions>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false; //нахер https!
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = authOptionsConfiguration.Issure,
+
+                    ValidateAudience = true,
+                    ValidAudience = authOptionsConfiguration.Audience,
+
+                    ValidateLifetime = true,
+
+                    IssuerSigningKey = authOptionsConfiguration.GetSymmetricSecurityKey(),//HS256
+                   ValidateIssuerSigningKey = true,
+                };
+            });
+
+
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+
+            services.AddSingleton(new List<Product>());
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            app.UseRouting();
+            app.UseCors();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers(); 
+                //endpoints.MapDefaultControllerRoute();
+            });
+        }
+
+    }
+}
